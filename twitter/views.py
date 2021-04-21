48effa95b1cd32f5c41a0248fb40f74527497a2d
Ben Hampson <bjhampson@gmail.com>
2021-04-21 19:36:27 +0100

    feat: ability to write a tweet at the top of the home page

--- twitter/views.py
+++ twitter/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 from .models import Tweet
+from .forms import TweetForm
 
 # Create your views here.
 class TweetListView(ListView):
@@ -9,3 +10,15 @@ class TweetListView(ListView):
     template_name = 'twitter/tweet_list.html'
     context_object_name = 'tweets'
     ordering = '-date_created'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = TweetForm()
+        return context
+    
+    def post(self, request):
+        form = TweetForm(request.POST)
+        form.instance.author = request.user
+        if form.is_valid():
+            form.save()
+        return redirect('home')
