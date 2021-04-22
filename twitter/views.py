284df71486350328df9d61d1d9d0a5d0ddafb465
Ben Hampson <bjhampson@gmail.com>
2021-04-22 12:34:41 +0100

    feat: tweet detail view + user public profile

--- twitter/views.py
+++ twitter/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, DetailView
 from .models import Tweet
 from .forms import TweetForm
+from django.contrib.auth.models import User
 
 # Create your views here.
 class TweetListView(ListView):
@@ -21,4 +22,17 @@ class TweetListView(ListView):
         form.instance.author = request.user
         if form.is_valid():
             form.save()
-        return redirect('home')
+        return redirect('tweet-detail')
+
+class TweetDetailView(DetailView):
+    model = Tweet
+    context_object_name = 'tweet'
+
+class UserTweetListView(ListView):
+    model = Tweet
+    template_name = 'twitter/user_tweets.html'
+    context_object_name = 'tweets'
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Tweet.objects.filter(author=user).order_by('-date_created')
