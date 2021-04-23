8d9fff387fe8733ef356e4e2cdf3cdac803ce23c
Ben Hampson <bjhampson@gmail.com>
2021-04-23 21:57:40 +0100

    fix: rectify ALLOWED_HOSTS to prevent status code 400

--- django_twitter/settings.py
+++ django_twitter/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'https://django-twitter-clone.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1', 'django-twitter-clone.herokuapp.com']
 
 
 # Application definition
