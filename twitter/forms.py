544188c3f6a7e3cf6f3733d59ef4d488ce4621af
Ben Hampson <bjhampson@gmail.com>
2021-05-03 19:08:53 +0100

    home page design improvements

--- twitter/forms.py
+++ twitter/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, Textarea
 from .models import Tweet
 
 class TweetForm(ModelForm):
@@ -6,3 +6,9 @@ class TweetForm(ModelForm):
     class Meta:
         model = Tweet
         fields = ['message']
+        widgets = {
+            'message': Textarea(attrs={
+                'class': 'textarea is-info has-fixed-size',
+                'rows': '2',
+                'placeholder': "What's happening?"}),
+        }
